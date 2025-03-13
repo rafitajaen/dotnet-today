2c53a6a7198cbf0ef3eb94498daca8f15533d172
Rafael Jaén <rafitajaen@gmail.com>
2025-03-13 18:26:25 +0100

    WIP(main): Include resources in rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,14 +3,21 @@ import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
+
+	const posts = await getCollection('blog').map((post) => ({
+		...post.data,
+		link: `/blog/${post.id}/`,
+	}));
+	
+	const resources = await getCollection('resources').map((resource) => ({
+		...resource.data,
+		link: `/resources/${resource.id}/`,
+	}));
+	
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
-		})),
+		items: [...posts, ...resources],
 	});
 }
